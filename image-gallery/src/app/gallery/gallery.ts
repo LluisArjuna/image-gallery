af673dd057cd6019abff807d9125ece96ec648b7
LluisArjuna <lluisarjunav@gmail.com>
2026-02-17 13:05:58 +0100

    feat: create the image gallery array using Image Interface

--- image-gallery/src/app/gallery/gallery.ts
+++ image-gallery/src/app/gallery/gallery.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Image } from '../interfaces/image-interface';
 
 @Component({
   selector: 'app-gallery',
@@ -6,4 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './gallery.html',
   styleUrl: './gallery.css',
 })
-export class Gallery {}
+export class Gallery {
+  imageGallery: Image[] = [{
+      id: '1',
+      src: 'https://picsum.photos/id/237/200/300',
+      alt: "Image of a black dog's face."
+    },
+    {
+      id: '2',
+      src: 'https://picsum.photos/id/238/200/300',
+      alt: 'Image of the skyline of New York.'
+    },
+    {
+      id: '3',
+      src: 'https://picsum.photos/id/239/200/300',
+      alt: 'Image of a hand holding a Dandelion.'
+    }]
+}
