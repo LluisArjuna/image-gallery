779a86ebb8bea50377a0b34c01afa6ba9f92cbfb
LluisArjuna <lluisarjunav@gmail.com>
2026-02-18 10:56:16 +0100

    feat(image-item): add required Input Signal for Image data

--- image-gallery/src/app/image-item/image-item.ts
+++ image-gallery/src/app/image-item/image-item.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, signal } from '@angular/core';
+import { Image } from '../interfaces/image-interface';
 
 @Component({
   selector: 'app-image-item',
   imports: [],
   templateUrl: './image-item.html',
   styleUrl: './image-item.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ImageItem {}
+export class ImageItem {
+  image = input.required<Image>()
+}
