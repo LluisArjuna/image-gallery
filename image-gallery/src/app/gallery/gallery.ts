a8e0e0372b46e7ddd7ea864b90e0c8fc658425eb
LluisArjuna <lluisarjunav@gmail.com>
2026-02-19 12:31:14 +0100

    refactor(image-item): css restructuring to tailwind

--- image-gallery/src/app/gallery/gallery.ts
+++ image-gallery/src/app/gallery/gallery.ts
@@ -23,5 +23,33 @@ export class Gallery {
       id: '3',
       src: 'https://picsum.photos/id/239/200/300',
       alt: 'Image of a hand holding a Dandelion.'
+    },{
+      id: '1',
+      src: 'https://picsum.photos/id/237/200/300',
+      alt: "Image of a black dog's face."
+    },
+    {
+      id: '2',
+      src: 'https://picsum.photos/id/238/200/300',
+      alt: 'Image of the skyline of New York.'
+    },
+    {
+      id: '3',
+      src: 'https://picsum.photos/id/239/200/300',
+      alt: 'Image of a hand holding a Dandelion.'
+    },{
+      id: '1',
+      src: 'https://picsum.photos/id/237/200/300',
+      alt: "Image of a black dog's face."
+    },
+    {
+      id: '2',
+      src: 'https://picsum.photos/id/238/200/300',
+      alt: 'Image of the skyline of New York.'
+    },
+    {
+      id: '3',
+      src: 'https://picsum.photos/id/239/200/300',
+      alt: 'Image of a hand holding a Dandelion.'
     }];
 }
